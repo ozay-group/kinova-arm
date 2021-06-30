3dac57cfa9a8557c1ceda3590b7273611a7e8465
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-06-30 15:14:35 -0400

    Added in code which installs some Python modules to the Dockerfile. Connects the Docker Drake container's ports to the right ports on the local machine so that visualization can be seen from local browser.

--- shell-scripts/create-docker-container.sh
+++ shell-scripts/create-docker-container.sh
@@ -5,9 +5,10 @@
 # Pull the latest public Docker Container for Drake
 docker pull robotlocomotion/drake:latest
 
-
 # Docker build and run the container
 docker build -t drake-image-v0 docker/
 
-docker run -td --name drake-container drake-image-v0
+docker run -td --name drake-container \
+    -p 7000:7000 \
+    drake-image-v0
 
