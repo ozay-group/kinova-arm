f3efe56e2b5f3c37981b7be1071c8e26b32594f7
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-08-02 16:43:16 -0400

    Added full implementation of TransitionSystemState and AtomicProposition. Includes new function StringSliceToAPs().

--- transitionsystem.go
+++ transitionsystem.go
@@ -11,17 +11,17 @@ import (
 )
 
 type TransitionSystem struct {
-	S          []int
+	S          []TransitionSystemState
 	Act        []int
-	Transition map[int]int
+	Transition map[int][][]int
 	I          []int // Set of Initial States
-	AP         []string
-	L          map[int][]string
+	AP         []AtomicProposition
+	L          map[int][]AtomicProposition
 }
 
-type StateOfTransitionSystem struct {
+type TransitionSystemState struct {
 	Value  int
-	System TransitionSystem
+	System *TransitionSystem
 }
 
 /*
@@ -30,21 +30,67 @@ type StateOfTransitionSystem struct {
 		Retrieves a StateOfTransitionSystem object from the transition system
 		ts.
 */
-func (ts TransitionSystem) GetState(state_in int) (StateOfTransitionSystem, error) {
+func (ts TransitionSystem) GetState(state_in int) (TransitionSystemState, error) {
 
 	// Checking Inputs
 	if (state_in < 0) || (state_in > len(ts.S)) {
-		return StateOfTransitionSystem{Value: -1}, errors.New(fmt.Sprintf("The input to GetState (%v) is not a state from the target transition system.", state_in))
+		return TransitionSystemState{Value: -1}, errors.New(fmt.Sprintf("The input to GetState (%v) is not a state from the target transition system.", state_in))
 	}
 
 	// Return Simple State
-	return StateOfTransitionSystem{Value: state_in, System: ts}, nil
+	return TransitionSystemState{Value: state_in, System: &ts}, nil
 
 }
 
 type AtomicProposition struct {
+	Name string
 }
 
 /*
+StringSliceToAPs
+Description:
+	Transforms a slice of int variables into a list of AtomicPropositions
+*/
+func StringSliceToAPs(stringSlice []string) []AtomicProposition {
+	var APList []AtomicProposition
+	for _, apName := range stringSlice {
+		APList = append(APList, AtomicProposition{Name: apName})
+	}
 
- */
+	return APList
+}
+
+/*
+GetTransitionSystem
+Description:
+
+*/
+func GetTransitionSystem(stateIndicies []int, actionIndicies []int, transitionMap map[int][][]int, initialStateList []int, atomicPropositionsList []string, labelMap map[int][]string) (TransitionSystem, error) {
+	// Constants
+
+	// Algorithm
+	ts := TransitionSystem{
+		Act:        actionIndicies,
+		Transition: transitionMap,
+		I:          initialStateList,
+	}
+
+	// Create List of States
+	var S []TransitionSystemState //List Of States
+	for _, stateValue := range stateIndicies {
+		S = append(S, TransitionSystemState{Value: stateValue, System: &ts})
+	}
+	ts.S = S
+
+	// Create List of Atomic Propositions
+	ts.AP = StringSliceToAPs(atomicPropositionsList)
+
+	// Create Label Values
+	fullLabelMap := make(map[TransitionSystemState][]AtomicProposition)
+	for stateValue, associatedAPs := range labelMap {
+		tempState := TransitionSystemState{Value: stateValue, System: &ts}
+		fullLabelMap[tempState] = StringSliceToAPs(associatedAPs)
+	}
+
+	return ts, nil
+}
--- transitionsystem_test.go
+++ transitionsystem_test.go
@@ -1,11 +1,17 @@
 package OzayGroupExploration
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTransitionSystem_GetState1(t *testing.T) {
 	// Create Simple Transition System
-	ts0 := TransitionSystem{
-		S: []int{1, 2, 3, 4},
+	ts0 := TransitionSystem{}
+	ts0.S = []TransitionSystemState{
+		TransitionSystemState{1, &ts0},
+		TransitionSystemState{2, &ts0},
+		TransitionSystemState{3, &ts0},
+		TransitionSystemState{4, &ts0},
 	}
 
 	// Try to get a state which is outside of the allowable range.
@@ -17,8 +23,12 @@ func TestTransitionSystem_GetState1(t *testing.T) {
 
 func TestTransitionSystem_GetState2(t *testing.T) {
 	// Create Simple Transition System
-	ts0 := TransitionSystem{
-		S: []int{1, 2, 3, 4},
+	ts0 := TransitionSystem{}
+	ts0.S = []TransitionSystemState{
+		TransitionSystemState{1, &ts0},
+		TransitionSystemState{2, &ts0},
+		TransitionSystemState{3, &ts0},
+		TransitionSystemState{4, &ts0},
 	}
 
 	// Try to get a state which is outside of the allowable range.
@@ -29,5 +39,41 @@ func TestTransitionSystem_GetState2(t *testing.T) {
 
 	if tempState.Value != 2 {
 		t.Errorf("The value of the correct state (2) was not saved in the state.")
+	}
+}
+
+func TestTransitionSystem_GetTransitionSystem1(t *testing.T) {
+	ts0, err := GetTransitionSystem(
+		[]int{1, 2, 3}, []int{1, 2},
+		map[int][][]int{
+			1: [][]int{
+				[]int{1},
+				[]int{2},
+			},
+			2: [][]int{
+				[]int{1, 2},
+				[]int{2, 3},
+			},
+			3: [][]int{
+				[]int{3},
+				[]int{2},
+			},
+		},
+		[]int{1},
+		[]string{"A", "B", "C", "D"},
+		map[int][]string{
+			1: []string{"A"},
+			2: []string{"B", "D"},
+			3: []string{"C", "D"},
+		},
+	)
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	// Tests
+	if len(ts0.AP) != 4 {
+		t.Errorf("The number of atomic propositions was expected to be 4, but it was %v.", len(ts0.AP))
 	}
 }
