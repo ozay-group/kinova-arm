fc256380798fbf7122697945f881ad5542191185
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-08-09 23:23:47 -0400

    Created a Post() operator and tested it with several helper functions.

--- transitionsystem.go
+++ transitionsystem.go
@@ -12,38 +12,53 @@ import (
 
 type TransitionSystem struct {
 	S          []TransitionSystemState
-	Act        []int
-	Transition map[int][][]int
-	I          []int // Set of Initial States
+	Act        []string
+	Transition map[TransitionSystemState]map[string][]TransitionSystemState
+	I          []TransitionSystemState // Set of Initial States
 	AP         []AtomicProposition
 	L          map[TransitionSystemState][]AtomicProposition
 }
 
 type TransitionSystemState struct {
-	Value  int
+	Name   string
 	System *TransitionSystem
 }
 
-/*
-	GetState
-	Description:
-		Retrieves a StateOfTransitionSystem object from the transition system
-		ts.
-*/
-func (ts TransitionSystem) GetState(state_in int) (TransitionSystemState, error) {
-
-	// Checking Inputs
-	if (state_in < 0) || (state_in > len(ts.S)) {
-		return TransitionSystemState{Value: -1}, errors.New(fmt.Sprintf("The input to GetState (%v) is not a state from the target transition system.", state_in))
-	}
-
-	// Return Simple State
-	return TransitionSystemState{Value: state_in, System: &ts}, nil
-
+func (s1 TransitionSystemState) Equals(s2 TransitionSystemState) bool {
+	return s1.Name == s2.Name
 }
+
+// /*
+// 	GetState
+// 	Description:
+// 		Retrieves a StateOfTransitionSystem object from the transition system
+// 		ts.
+// */
+// func (ts TransitionSystem) GetState(stateNameIn string) (TransitionSystemState, error) {
+
+// 	// Checking Inputs
+// 	if (stateNameIn < 0) || (state_in > len(ts.S)) {
+// 		return TransitionSystemState{Name: ""}, errors.New(fmt.Sprintf("The input to GetState (%v) is not a state from the target transition system.", state_in))
+// 	}
+
+// 	// Return Simple State
+// 	return TransitionSystemState{Name: state_in, System: &ts}, nil
+
+// }
 
 type AtomicProposition struct {
 	Name string
+}
+
+/*
+Equals
+Description:
+	Compares the atomic propositions by their names.
+*/
+func (ap1 AtomicProposition) Equals(ap2 AtomicProposition) bool {
+
+	return ap1.Name == ap2.Name
+
 }
 
 /*
@@ -65,34 +80,176 @@ GetTransitionSystem
 Description:
 
 */
-func GetTransitionSystem(stateIndicies []int, actionIndicies []int, transitionMap map[int][][]int, initialStateList []int, atomicPropositionsList []string, labelMap map[int][]string) (TransitionSystem, error) {
+func GetTransitionSystem(stateNames []string, actionNames []string, transitionMap map[string]map[string][]string, initialStateList []string, atomicPropositionsList []string, labelMap map[string][]string) (TransitionSystem, error) {
 	// Constants
 
 	// Algorithm
 	ts := TransitionSystem{
-		Act:        actionIndicies,
-		Transition: transitionMap,
-		I:          initialStateList,
-		AP:         StringSliceToAPs(atomicPropositionsList),
+		Act: actionNames,
+		AP:  StringSliceToAPs(atomicPropositionsList),
 	}
 
 	// Create List of States
 	var S []TransitionSystemState //List Of States
-	for _, stateValue := range stateIndicies {
-		S = append(S, TransitionSystemState{Value: stateValue, System: &ts})
+	for _, stateName := range stateNames {
+		S = append(S, TransitionSystemState{Name: stateName, System: &ts})
 	}
 	ts.S = S
+
+	// Create List of Initial States
+	var I []TransitionSystemState
+	for _, stateName := range initialStateList {
+		I = append(I, TransitionSystemState{Name: stateName, System: &ts})
+	}
+	ts.I = I
 
 	// // Create List of Atomic Propositions
 	// ts.AP = StringSliceToAPs(atomicPropositionsList)
 
+	// Create Transition Map
+	Transition := make(map[TransitionSystemState]map[string][]TransitionSystemState)
+	for siName, perStateMap := range transitionMap {
+		si := TransitionSystemState{Name: siName, System: &ts}
+		tempActionMap := make(map[string][]TransitionSystemState)
+		for actionName, stateArray := range perStateMap {
+			var tempStates []TransitionSystemState
+			for _, siPlus1Name := range stateArray {
+				tempStates = append(tempStates, TransitionSystemState{Name: siPlus1Name, System: &ts})
+			}
+			tempActionMap[actionName] = tempStates
+		}
+		Transition[si] = tempActionMap
+	}
+	ts.Transition = Transition
+
 	// Create Label Values
 	fullLabelMap := make(map[TransitionSystemState][]AtomicProposition)
 	for stateValue, associatedAPs := range labelMap {
-		tempState := TransitionSystemState{Value: stateValue, System: &ts}
+		tempState := TransitionSystemState{Name: stateValue, System: &ts}
 		fullLabelMap[tempState] = StringSliceToAPs(associatedAPs)
 	}
 	ts.L = fullLabelMap
 
 	return ts, nil
+}
+
+/*
+Satisfies
+Description:
+	The state of the transition system satisfies the given formula.
+*/
+
+func (stateIn TransitionSystemState) Satisfies(formula interface{}) (bool, error) {
+
+	// Input Processing
+	if stateIn.System == nil {
+		return false, errors.New("The system pointer is not defined for the input state.")
+	}
+
+	// Algorithm
+	var tf = false
+	var err error = nil
+
+	if singleAP, ok := formula.(AtomicProposition); ok {
+		tf, err = singleAP.SatisfactionHelper(stateIn)
+	}
+
+	return tf, err
+}
+
+func (ap AtomicProposition) SatisfactionHelper(stateIn TransitionSystemState) (bool, error) {
+	// Constants
+
+	SystemPointer := stateIn.System
+	LOfState := SystemPointer.L[stateIn]
+
+	// Find If ap is in LOfState
+	tf := false
+	for _, tempAP := range LOfState {
+		tf = tf || tempAP.Equals(ap)
+	}
+
+	return tf, nil
+
+}
+
+/*
+AppendIfUnique
+Description:
+	Appends to the input slice sliceIn if and only if the new state
+	is actually a unique state.
+*/
+func AppendIfUnique(sliceIn []TransitionSystemState, stateIn TransitionSystemState) []TransitionSystemState {
+	// Check to see if the State is equal to any of the ones in the list.
+	for _, stateFromSlice := range sliceIn {
+		if stateFromSlice.Equals(stateIn) {
+			return sliceIn
+		}
+	}
+
+	// If none of the states in sliceIn are equal to stateIn, then return the appended version of sliceIn.
+	return append(sliceIn, stateIn)
+
+}
+
+/*
+Post
+Description:
+	Finds the set of states that can follow a given state (or set of states).
+Usage:
+
+*/
+
+func Post(SorSA ...interface{}) ([]TransitionSystemState, error) {
+	switch len(SorSA) {
+	case 1:
+		// Only State Is Given
+		stateIn, ok := SorSA[0].(TransitionSystemState)
+		if !ok {
+			return []TransitionSystemState{}, errors.New("The first input to post is not of type TransitionSystemState.")
+		}
+
+		System := stateIn.System
+		allActions := System.Act
+
+		var nextStates []TransitionSystemState
+		var tempPost []TransitionSystemState
+		var err error
+		for _, action := range allActions {
+			tempPost, err = Post(stateIn, action)
+			if err != nil {
+				return []TransitionSystemState{}, err
+			}
+			for _, postElt := range tempPost {
+				nextStates = AppendIfUnique(nextStates, postElt)
+			}
+		}
+
+		return nextStates, nil
+
+	case 2:
+		// State and Action is Given
+		stateIn, ok := SorSA[0].(TransitionSystemState)
+		if !ok {
+			return []TransitionSystemState{}, errors.New("The first input to post is not of type TransitionSystemState.")
+		}
+
+		actionIn, ok := SorSA[1].(string)
+		if !ok {
+			return []TransitionSystemState{}, errors.New("The second input to post is not of type string!")
+		}
+
+		// Get Transition value
+		System := stateIn.System
+		tValues := System.Transition[stateIn][actionIn]
+		var nextStates []TransitionSystemState
+		for _, nextState := range tValues {
+			nextStates = AppendIfUnique(nextStates, nextState)
+		}
+
+		return nextStates, nil
+	}
+
+	// Return error
+	return []TransitionSystemState{}, errors.New(fmt.Sprintf("Unexpected number of inputs to post (%v).", len(SorSA)))
 }
--- transitionsystem_test.go
+++ transitionsystem_test.go
@@ -8,16 +8,15 @@ func TestTransitionSystem_GetState1(t *testing.T) {
 	// Create Simple Transition System
 	ts0 := TransitionSystem{}
 	ts0.S = []TransitionSystemState{
-		TransitionSystemState{1, &ts0},
-		TransitionSystemState{2, &ts0},
-		TransitionSystemState{3, &ts0},
-		TransitionSystemState{4, &ts0},
+		TransitionSystemState{"1", &ts0},
+		TransitionSystemState{"2", &ts0},
+		TransitionSystemState{"3", &ts0},
+		TransitionSystemState{"4", &ts0},
 	}
 
 	// Try to get a state which is outside of the allowable range.
-	_, err := ts0.GetState(5)
-	if err.Error() != "The input to GetState (5) is not a state from the target transition system." {
-		t.Errorf("The correct error was not produced when calling GetState().")
+	if len(ts0.S) == 4 {
+		t.Errorf("There are not four states left.")
 	}
 }
 
@@ -25,46 +24,42 @@ func TestTransitionSystem_GetState2(t *testing.T) {
 	// Create Simple Transition System
 	ts0 := TransitionSystem{}
 	ts0.S = []TransitionSystemState{
-		TransitionSystemState{1, &ts0},
-		TransitionSystemState{2, &ts0},
-		TransitionSystemState{3, &ts0},
-		TransitionSystemState{4, &ts0},
+		TransitionSystemState{"1", &ts0},
+		TransitionSystemState{"2", &ts0},
+		TransitionSystemState{"3", &ts0},
+		TransitionSystemState{"4", &ts0},
 	}
 
 	// Try to get a state which is outside of the allowable range.
-	tempState, err := ts0.GetState(2)
-	if err != nil {
-		t.Errorf("The correct error was not produced when calling GetState().")
-	}
-
-	if tempState.Value != 2 {
+	tempState := ts0.S[1]
+	if tempState.Name != "2" {
 		t.Errorf("The value of the correct state (2) was not saved in the state.")
 	}
 }
 
 func TestTransitionSystem_GetTransitionSystem1(t *testing.T) {
 	ts0, err := GetTransitionSystem(
-		[]int{1, 2, 3}, []int{1, 2},
-		map[int][][]int{
-			1: [][]int{
-				[]int{1},
-				[]int{2},
+		[]string{"1", "2", "3"}, []string{"1", "2"},
+		map[string]map[string][]string{
+			"1": map[string][]string{
+				"1": []string{"1"},
+				"2": []string{"2"},
 			},
-			2: [][]int{
-				[]int{1, 2},
-				[]int{2, 3},
+			"2": map[string][]string{
+				"1": []string{"1", "2"},
+				"2": []string{"2", "3"},
 			},
-			3: [][]int{
-				[]int{3},
-				[]int{2},
+			"3": map[string][]string{
+				"1": []string{"3"},
+				"2": []string{"2"},
 			},
 		},
-		[]int{1},
+		[]string{"1"},
 		[]string{"A", "B", "C", "D"},
-		map[int][]string{
-			1: []string{"A"},
-			2: []string{"B", "D"},
-			3: []string{"C", "D"},
+		map[string][]string{
+			"1": []string{"A"},
+			"2": []string{"B", "D"},
+			"3": []string{"C", "D"},
 		},
 	)
 
@@ -76,4 +71,157 @@ func TestTransitionSystem_GetTransitionSystem1(t *testing.T) {
 	if len(ts0.AP) != 4 {
 		t.Errorf("The number of atomic propositions was expected to be 4, but it was %v.", len(ts0.AP))
 	}
+}
+
+/*
+TestTransitionSystem_Equals1
+Description:
+	Tests if the Equals() member function for AtomicProposition works.
+*/
+func TestTransitionSystem_Equals1(t *testing.T) {
+	// Constants
+	ap1 := AtomicProposition{Name: "A"}
+	ap2 := AtomicProposition{Name: "B"}
+	ap3 := AtomicProposition{Name: "A"}
+
+	if ap1.Equals(ap2) {
+		t.Errorf("ap1 (%v) is supposed to be different from ap2 (%v).", ap1.Name, ap2.Name)
+	}
+
+	if !ap1.Equals(ap3) {
+		t.Errorf("ap1 (%v) is supposed to be the same as ap3 (%v).", ap1.Name, ap3.Name)
+	}
+
+}
+
+/*
+GetSimpleTS
+Description:
+	Get a transition system to test satisfies.
+*/
+func GetSimpleTS1() TransitionSystem {
+	ts0, _ := GetTransitionSystem(
+		[]string{"1", "2", "3"}, []string{"1", "2"},
+		map[string]map[string][]string{
+			"1": map[string][]string{
+				"1": []string{"1"},
+				"2": []string{"2"},
+			},
+			"2": map[string][]string{
+				"1": []string{"1", "2"},
+				"2": []string{"2", "3"},
+			},
+			"3": map[string][]string{
+				"1": []string{"3"},
+				"2": []string{"2"},
+			},
+		},
+		[]string{"1"},
+		[]string{"A", "B", "C", "D"},
+		map[string][]string{
+			"1": []string{"A"},
+			"2": []string{"B", "D"},
+			"3": []string{"C", "D"},
+		},
+	)
+
+	return ts0
+}
+
+/*
+TestTransitionSystem_Satisfies1
+Description:
+	Tests if the Satisfies() member function correctly identifies when the system
+	satisfies a given transition system.
+*/
+func TestTransitionSystem_Satisfies1(t *testing.T) {
+	// Constants
+	ts1 := GetSimpleTS1()
+	ap2 := AtomicProposition{Name: "B"}
+
+	// Test
+	state2 := ts1.S[1]
+
+	tf, err := state2.Satisfies(ap2)
+	if err != nil {
+		t.Errorf("There was an error while testing satisfies: %v", err.Error())
+	}
+
+	if !tf {
+		t.Errorf("ap1 (%v) is supposed to be satisfied by ts1.", ap2.Name)
+	}
+
+}
+
+/*
+TestTransitionSystem_Satisfies2
+Description:
+	Tests if the Satisfies() member function correctly identifies when the system
+	satisfies a given transition system.
+*/
+func TestTransitionSystem_Satisfies2(t *testing.T) {
+	// Constants
+	ts1 := GetSimpleTS1()
+	ap2 := AtomicProposition{Name: "B"}
+
+	// Test
+	state2 := ts1.S[0]
+
+	tf, err := state2.Satisfies(ap2)
+	if err != nil {
+		t.Errorf("There was an error while testing satisfies: %v", err.Error())
+	}
+
+	if tf {
+		t.Errorf("ap1 (%v) is supposed to NOT be satisfied by ts1.", ap2.Name)
+	}
+
+}
+
+/*
+TestTransitionSystemState_Post1
+Description:
+	Tests if the Post() member function correctly identifies when the system
+	satisfies a given transition system.
+*/
+func TestTransitionSystemState_Post1(t *testing.T) {
+	// Constants
+	ts1 := GetSimpleTS1()
+
+	// Test
+	state2 := ts1.S[1]
+
+	nextStates, err := Post(state2, "1")
+	if err != nil {
+		t.Errorf("There was an error while testing Post: %v", err.Error())
+	}
+
+	if len(nextStates) != 2 {
+		t.Errorf("Expected there to be 2 options for next state but received %v options. %v", len(nextStates), nextStates[0])
+	}
+
+}
+
+/*
+TestTransitionSystemState_Post2
+Description:
+	Tests if the Post() member function correctly creates the ancestors
+	when there is no action given.
+*/
+func TestTransitionSystemState_Post2(t *testing.T) {
+	// Constants
+	ts1 := GetSimpleTS1()
+
+	// Test
+	state2 := ts1.S[1]
+
+	nextStates, err := Post(state2)
+	if err != nil {
+		t.Errorf("There was an error while testing Post: %v", err.Error())
+	}
+
+	if len(nextStates) != 3 {
+		t.Errorf("Expected there to be 3 options for next state but received %v options. %v", len(nextStates), nextStates[0])
+	}
+
 }
