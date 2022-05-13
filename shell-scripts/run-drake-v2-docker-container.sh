19516c87eed247b685e3bf61b455c1ee662ad93e
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2022-05-12 20:10:19 -0400

    Reinstated the old docker container setup instructions in the shell script. Helps to support X11 forwarding.

--- shell-scripts/run-drake-v2-docker-container.sh
+++ shell-scripts/run-drake-v2-docker-container.sh
@@ -1,14 +1,16 @@
-# run-drake-docker-container.sh
+# run-drake-docker-v2-container.sh
 # Description:
-#   
+#   This script was meant to enable users of Mac OS X to:
+#   - spin up a container using the image saved as drake-image-v2
+#   - incorporate X11 forwarding from the container to the Mac (requires that you run `xhost +` in the XQuartz terminal)
 
-IP1=35.3.126.205
+export IP1=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 
 docker run -td --name drake-container2 \
-    -e DISPLAY=$DISPLAY \
+    -e DISPLAY=$IP1:0 \
     -e XAUTHORITY=/.Xauthority \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -p 7000:7000 \
-    -network="host" \
+    -p 7001:7000 \
+    --network="host" \
     drake-image-v2
 
