634bce0fe9ba1ce17862c41b8667fe32807d3de9
kwesiRutledge <krutledg@umich.edu>
2022-06-09 07:23:08 +0000

    Initial commit: build components for 3D calibration.

--- vision_module/kinova_vision_example.py
+++ vision_module/kinova_vision_example.py
@@ -6,6 +6,25 @@ import numpy as np
 color_cap = cv2.VideoCapture("rtsp://192.168.1.10/color", cv2.CAP_FFMPEG)
 depth_cap = cv2.VideoCapture("rtsp://192.168.1.10/depth")
 
+# Get the camera intrinsics from "01-vision_intrinsics.py" by KINOVA (R) KORTEX (TM). 
+# Make sure you have the repository "kortex"
+import kortex_vision_intrinsics as kv_in
+color_intrinsics, depth_intrinsics = kv_in.main()
+#print(color_intrinsics)
+#print(depth_intrinsics)
+
+"""TODO: # Get the forward transformation of the kinova arm.
+import kortex_compute_kinematics as kck
+joint_angle, forward_kinematics = kck.main()
+print(joint_angle)
+print(forward_kinematics)"""
+
+from datetime import datetime
+import os
+directory_path = "/root/OzayGroupExploration/vision_module/image_from_kinova"
+
+num_frame = 0
+
 try:
     while(1):
         # These frames are expressed in the camera frame. 
@@ -33,6 +52,18 @@ try:
         cv2.imshow('Kinova Depth Camera', images)
         cv2.waitKey(1)
 
+        if num_frame == 10:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            color_file = current_time + "_color.png"
+            color_path = os.path.join(directory_path,color_file)
+            cv2.imwrite(color_path, color_image)
+            depth_file = current_time + "_depth.png"
+            depth_path = os.path.join(directory_path,depth_file)
+            cv2.imwrite(depth_path, depth_image)
+            break
+        num_frame = num_frame + 1
+
 finally:
     color_cap.release()
     depth_cap.release()
@@ -43,5 +74,4 @@ https://stackoverflow.com/questions/59590200/generate-point-cloud-from-depth-ima
 Inspiration: https://www.mathworks.com/help/supportpkg/robotmanipulator/ug/generate-colorized-point-cloud-gen3.html
 https://stackoverflow.com/questions/40875846/capturing-rtsp-camera-using-opencv-python
 https://github.com/Kinovarobotics/kortex/issues/112
-https://www.mathworks.com/help/supportpkg/robotmanipulator/ug/generate-colorized-point-cloud-gen3.html
 """
