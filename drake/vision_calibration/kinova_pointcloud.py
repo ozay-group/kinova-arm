07df44da72abfcd2ce4b72b5dd16164dfbd0e610
daixingze <xngzdai@umich.edu>
2022-07-27 18:54:49 +0000

    Solved "no points in point cloud" issue.

--- drake/vision_calibration/kinova_pointcloud.py
+++ drake/vision_calibration/kinova_pointcloud.py
@@ -13,29 +13,34 @@ def main():
     depth_image = o3d.io.read_image("depth_image.png")
 
     ## Generate an RGBD image (open3d)
-    rgbd_image = o3d.geometry.RGBDImage()
-    rgbd_image.create_from_color_and_depth(color_image, depth_image)
+    print("Generate an RGBD image (open3d)")
+    rgbd_image = o3d.geometry.RGBDImage().create_from_color_and_depth(color_image, depth_image)
 
 
     ## Load wrist camera extrinsic properties
+    print("Load wrist camera extrinsic properties")
     extrinsic = np.load('forward_kinematics.npy')
 
     ## Load camera intrinsic properties
+    print("Load camera intrinsic properties")
     color_intrinsic = np.load('color_general_intrinsic_parameters.npy')
     depth_intrinsic = np.load('depth_general_intrinsic_parameters.npy')
     #TODO: which intrinsic properties are needed?
     intrinsic_param = color_intrinsic #TODO: temporary solution.
     
     ## Set the intrinsic properties
+    print("Set the intrinsic properties")
     intrinsic = o3d.camera.PinholeCameraIntrinsic()
-    intrinsic.set_intrinsics(width=intrinsic_param[0], height=intrinsic_param[1], 
+    intrinsic.set_intrinsics(width=int(intrinsic_param[0]), height=int(intrinsic_param[1]), 
                             fx=intrinsic_param[2], fy=intrinsic_param[3], 
-                            cx=intrinsic_param[4], cy=intrinsic_param[6])
+                            cx=intrinsic_param[4], cy=intrinsic_param[5])
 
     ## Generate a point cloud model
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.create_from_rgbd_image(rgbd_image, intrinsic, extrinsic)
-    
+    print("Generate a point cloud model")
+    point_cloud = o3d.geometry.PointCloud().create_from_rgbd_image(rgbd_image, intrinsic, extrinsic)
+    # Flip it, otherwise the pointcloud will be upside down.
+    point_cloud.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
     return point_cloud
 
 if __name__ == "__main__":
