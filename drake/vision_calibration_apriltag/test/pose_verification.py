2fcff0b38edefe541e1872899e201fbaf3438218
daixingze <xngzdai@umich.edu>
2022-07-29 19:16:57 +0000

    Modified matrix calculations.

--- drake/vision_calibration_apriltag/test/pose_verification.py
+++ drake/vision_calibration_apriltag/test/pose_verification.py
@@ -1,12 +1,17 @@
 import numpy as np
 from cv2 import Rodrigues
 
-cube_pose = np.load('forward_kinematics.npy')
+X_WorldCube = np.load('forward_kinematics.npy')
 
-measured_pose = np.array([[0.0, 0.0, 0.0, 0.0],]) # Hard coded for now
+X_WorldRealsense = np.load('kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
+
+X_RealsenseCube = np.array([[0.0, 0.0, 0.0, 0.0],]) # Hard coded for now
+
+measured_X_WorldCube = X_WorldRealsense @ X_RealsenseCube
 
 pose_err = 1e-3 # Hard coded for now
 
 # https://stackoverflow.com/a/42723571
-r, _ = Rodrigues(cube_pose.dot(measured_pose.T))
-transformation_error_from_identity = np.linalg.norm(r)
+v, _ = Rodrigues(X_WorldCube.dot(measured_X_WorldCube.T))
+transformation_error_from_identity = np.linalg.norm(v)
+print('Transformation error from identity:', transformation_error_from_identity)
