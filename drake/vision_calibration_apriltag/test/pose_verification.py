36b8f79cbbe6013d4e324cac10e3a2a8aed07eeb
daixingze <xngzdai@umich.edu>
2022-07-29 19:24:13 +0000

    Modified matrix calculation.

--- drake/vision_calibration_apriltag/test/pose_verification.py
+++ drake/vision_calibration_apriltag/test/pose_verification.py
@@ -5,7 +5,14 @@ X_WorldCube = np.load('forward_kinematics.npy')
 
 X_WorldRealsense = np.load('kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
 
-X_RealsenseCube = np.array([[0.0, 0.0, 0.0, 0.0],]) # Hard coded for now
+R_RealsenseCube = np.array([[ 0.59212563,  0.01627912, -0.80568122],
+ [-0.28292787,  0.94034869, -0.18893427],
+ [ 0.7545456,   0.3398225,   0.56141038]])
+t_RealsenseCube = np.array([[-0.12280271],
+ [ 0.02189387],
+ [ 0.5466977 ]])
+X_RealsenseCube = np.concatenate((R_RealsenseCube, t_RealsenseCube), axis=1)
+X_RealsenseCube = np.concatenate((X_RealsenseCube, np.array([[0, 0, 0, 1]])), axis=0)
 
 measured_X_WorldCube = X_WorldRealsense @ X_RealsenseCube
 
