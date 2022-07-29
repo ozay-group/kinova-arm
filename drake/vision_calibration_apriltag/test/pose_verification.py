4bb7e853f82027c05ca186c3149d3e5db9f1f947
kwesiRutledge <krutledg@umich.edu>
2022-07-29 20:27:43 +0000

    Xingze: Onsite testing.

--- drake/vision_calibration_apriltag/test/pose_verification.py
+++ drake/vision_calibration_apriltag/test/pose_verification.py
@@ -1,24 +1,52 @@
 import numpy as np
 from cv2 import Rodrigues
 
-X_WorldCube = np.load('forward_kinematics.npy')
+X_WorldEndeffector = np.load('forward_kinematics.npy')
+print(X_WorldEndeffector)
 
-X_WorldRealsense = np.load('kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
+# According to the User Guide Gen3, if the Robotiq 2F-85 Gripper is installed,
+# the transformation from interface module frame to tool frame is:
+# x = 0, y = 0, z = 0.120m, theta_x = 0, theta_y = 0, theta_z = 0.
+z = 0.120
+# The size of the Apriltag cube is 3 inches, 0.0762m
+a = 0.0762
+# If the opposite face of the Apriltag matches the center of the gripper and
+# it is kept parallel to the interface.
+z_offset = z + a
+X_EndeffectorApriltag = np.zeros((4,4))
+X_EndeffectorApriltag[2,3] = z_offset
 
-R_RealsenseCube = np.array([[ 0.59212563,  0.01627912, -0.80568122],
- [-0.28292787,  0.94034869, -0.18893427],
- [ 0.7545456,   0.3398225,   0.56141038]])
-t_RealsenseCube = np.array([[-0.12280271],
- [ 0.02189387],
- [ 0.5466977 ]])
-X_RealsenseCube = np.concatenate((R_RealsenseCube, t_RealsenseCube), axis=1)
-X_RealsenseCube = np.concatenate((X_RealsenseCube, np.array([[0, 0, 0, 1]])), axis=0)
+X_WorldApriltag = X_WorldEndeffector + X_EndeffectorApriltag
 
-measured_X_WorldCube = X_WorldRealsense @ X_RealsenseCube
+print(X_WorldApriltag)
 
-pose_err = 1e-3 # Hard coded for now
+X_WorldRealsense = np.load('/root/kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
+
+R_RealsenseApriltag = np.array([[ 0.99559036,  0.08261408,  0.04443809],
+ [-0.07829508,  0.99272777, -0.09144102],
+ [-0.05166924,  0.08755852,  0.99481847]])
+t_RealsenseApriltag = np.array([[0.07921371],
+ [-0.12779749],
+ [ 0.37730513]])
+X_RealsenseApriltag = np.concatenate((R_RealsenseApriltag, t_RealsenseApriltag), axis=1)
+X_RealsenseApriltag = np.concatenate((X_RealsenseApriltag, np.array([[0, 0, 0, 1]])), axis=0)
+
+measured_X_WorldApriltag = X_WorldRealsense @ X_RealsenseApriltag
+
+pose_err = 3.619507164899972e-08 # Hard coded for now
 
 # https://stackoverflow.com/a/42723571
-v, _ = Rodrigues(X_WorldCube.dot(measured_X_WorldCube.T))
-transformation_error_from_identity = np.linalg.norm(v)
+#v, _ = Rodrigues(X_WorldCube.dot(measured_X_WorldCube.T))
+#
+m = X_WorldApriltag @ measured_X_WorldApriltag.T
+print(m)
+transformation_error_from_identity = np.linalg.norm(m)
+print('Transformation error from identity:', transformation_error_from_identity)
+
+dm = X_WorldApriltag - measured_X_WorldApriltag
+print(dm)
+
+m = X_WorldApriltag.T @ measured_X_WorldApriltag
+print(m)
+transformation_error_from_identity = np.linalg.norm(m)
 print('Transformation error from identity:', transformation_error_from_identity)
