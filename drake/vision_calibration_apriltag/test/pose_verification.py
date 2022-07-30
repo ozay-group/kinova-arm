8dc2ef2e936dc6c8c626be3c1e914e523282df40
daixingze <xngzdai@umich.edu>
2022-07-30 06:58:30 +0000

    Improved readability.

--- drake/vision_calibration_apriltag/test/pose_verification.py
+++ drake/vision_calibration_apriltag/test/pose_verification.py
@@ -1,36 +1,39 @@
+"""POSE_VERIFICATION
+    Summary:
+        This script is used to verify the pose of the Intel Realsense camera.
+"""
+
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-from cv2 import Rodrigues
 
+## Load the transform of the end effector.
 X_WorldEndeffector = np.load('forward_kinematics.npy')
-print(X_WorldEndeffector)
+#print(X_WorldEndeffector)
 
-# According to the User Guide Gen3, if the Robotiq 2F-85 Gripper is installed,
-# the transformation from interface module frame to tool frame is:
-# x = 0, y = 0, z = 0.120m, theta_x = 0, theta_y = 0, theta_z = 0.
+## Split the transformation (4,4) into rotation (3,3) and translation (3,1)
+t_WorldEndeffector = X_WorldEndeffector[:3,3]
+R_WorldEndeffector = X_WorldEndeffector[:3,:3]
+
+## According to the User Guide Gen3, if the Robotiq 2F-85 Gripper is installed,
+## the transformation from interface module frame to tool frame is:
+## x = 0, y = 0, z = 0.120m, theta_x = 0, theta_y = 0, theta_z = 0.
 z = 0.120
-# The size of the Apriltag cube is 3 inches, 0.0762m
+
+## The size of the Apriltag cube is 3 inches, 0.0762m
 a = 0.0762
-# If the opposite face of the Apriltag matches the center of the gripper and
-# it is kept parallel to the interface.
-z_offset = z + a
-X_EndeffectorApriltag = np.zeros((4,4))
-X_EndeffectorApriltag[2,3] = z_offset
 
-X_WorldApriltag = X_WorldEndeffector + X_EndeffectorApriltag
-#print(X_WorldApriltag)
+## If the opposite face of the Apriltag matches the center of the gripper and
+## it is kept parallel to the interface.
+t_offset = np.array([0, 0, z + a])
 
-R_WorldApriltag = X_WorldApriltag[:3,:3]
-print(R_WorldApriltag)
-RR_WorldApriltag = R.from_matrix(R_WorldApriltag)
-rv = RR_WorldApriltag.as_euler('xyz')
-print(rv)
+## Find the pose of the Apriltag in the world frame. 
+t_WorldApriltag = t_WorldEndeffector + t_offset
+r_WorldApriltag = R.from_matrix(R_WorldEndeffector).as_euler('xyz') # numpy array (1,3)
 
-t_WorldApriltag = X_WorldApriltag[:3,3]
-print(t_WorldApriltag)
-
+## Load the measured pose of the Intel Realsense camera.
 X_WorldRealsense = np.load('/root/kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
 
+## Hard code the rotation matrix and translation vector from "realsense_result.txt"
 R_RealsenseApriltag = np.array([[ 0.99559036,  0.08261408,  0.04443809],
  [-0.07829508,  0.99272777, -0.09144102],
  [-0.05166924,  0.08755852,  0.99481847]])
@@ -40,41 +43,23 @@ t_RealsenseApriltag = np.array([[0.07921371],
 X_RealsenseApriltag = np.concatenate((R_RealsenseApriltag, t_RealsenseApriltag), axis=1)
 X_RealsenseApriltag = np.concatenate((X_RealsenseApriltag, np.array([[0, 0, 0, 1]])), axis=0)
 
+## Calculate the pose of the Apriltag based on the measured pose of the Intel Realsense camera.
 measured_X_WorldApriltag = X_WorldRealsense @ X_RealsenseApriltag
 
-measured_R_WorldApriltag = measured_X_WorldApriltag[:3,:3]
-print(measured_R_WorldApriltag)
-measured_RR_WorldApriltag = R.from_matrix(measured_R_WorldApriltag)
-measured_rv = measured_RR_WorldApriltag.as_euler('xyz')
-print(measured_rv)
-
+## Split the transformation (4,4) into rotation (3,3) and translation (3,1)
 measured_t_WorldApriltag = measured_X_WorldApriltag[:3,3]
-print(measured_t_WorldApriltag)
+measured_R_WorldApriltag = measured_X_WorldApriltag[:3,:3]
+measured_r_WorldApriltag = R.from_matrix(measured_R_WorldApriltag).as_euler('xyz')
 
-pose_err = 3.619507164899972e-08 # Hard coded for now
+pose_err = 3.619507164899972e-08 # Hard coded from "realsense_result.txt"
 
-# https://stackoverflow.com/a/42723571
-#v, _ = Rodrigues(X_WorldCube.dot(measured_X_WorldCube.T))
-#
-"""m = X_WorldApriltag @ measured_X_WorldApriltag.T
-print(m)
-transformation_error_from_identity = np.linalg.norm(m)
-print('Transformation error from identity:', transformation_error_from_identity)
+## Calculate the error between the measured pose and the true pose.
+t_err = (t_WorldApriltag - measured_t_WorldApriltag)
+print("Translation error: ", t_err)
+print("Translation error norm: ", np.linalg.norm(t_err))
 
-dm = X_WorldApriltag - measured_X_WorldApriltag
-print(dm)
-
-m = X_WorldApriltag.T @ measured_X_WorldApriltag
-print(m)
-transformation_error_from_identity = np.linalg.norm(m)
-print('Transformation error from identity:', transformation_error_from_identity)"""
-
-r = np.linalg.norm(R_WorldApriltag @ measured_R_WorldApriltag.T)
-print(r)
-
-t = np.linalg.norm(t_WorldApriltag - measured_t_WorldApriltag)
-print(t)
-
-print(rv - measured_rv)
-rv_norm = np.linalg.norm(rv - measured_rv, 1) / 3
-print(rv_norm)
+r_tolerance = np.ones(3) * np.pi / 18
+r_err = np.abs(r_WorldApriltag - measured_r_WorldApriltag)
+print("Rotation error: ", r_err)
+r_bool = r_err / r_tolerance
+print("Rotation error is __ times of pi/18: ", r_bool)
---
+++ drake/vision_calibration_apriltag/test/pose_verification_workspace.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+from cv2 import Rodrigues
+
+X_WorldEndeffector = np.load('forward_kinematics.npy')
+print(X_WorldEndeffector)
+
+# According to the User Guide Gen3, if the Robotiq 2F-85 Gripper is installed,
+# the transformation from interface module frame to tool frame is:
+# x = 0, y = 0, z = 0.120m, theta_x = 0, theta_y = 0, theta_z = 0.
+z = 0.120
+# The size of the Apriltag cube is 3 inches, 0.0762m
+a = 0.0762
+# If the opposite face of the Apriltag matches the center of the gripper and
+# it is kept parallel to the interface.
+z_offset = z + a
+X_EndeffectorApriltag = np.zeros((4,4))
+X_EndeffectorApriltag[2,3] = z_offset
+
+X_WorldApriltag = X_WorldEndeffector + X_EndeffectorApriltag
+#print(X_WorldApriltag)
+
+R_WorldApriltag = X_WorldApriltag[:3,:3]
+print(R_WorldApriltag)
+RR_WorldApriltag = R.from_matrix(R_WorldApriltag)
+rv = RR_WorldApriltag.as_euler('xyz')
+print(rv)
+
+t_WorldApriltag = X_WorldApriltag[:3,3]
+print(t_WorldApriltag)
+
+X_WorldRealsense = np.load('/root/kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
+
+R_RealsenseApriltag = np.array([[ 0.99559036,  0.08261408,  0.04443809],
+ [-0.07829508,  0.99272777, -0.09144102],
+ [-0.05166924,  0.08755852,  0.99481847]])
+t_RealsenseApriltag = np.array([[0.07921371],
+ [-0.12779749],
+ [ 0.37730513]])
+X_RealsenseApriltag = np.concatenate((R_RealsenseApriltag, t_RealsenseApriltag), axis=1)
+X_RealsenseApriltag = np.concatenate((X_RealsenseApriltag, np.array([[0, 0, 0, 1]])), axis=0)
+
+measured_X_WorldApriltag = X_WorldRealsense @ X_RealsenseApriltag
+
+measured_R_WorldApriltag = measured_X_WorldApriltag[:3,:3]
+print(measured_R_WorldApriltag)
+measured_RR_WorldApriltag = R.from_matrix(measured_R_WorldApriltag)
+measured_rv = measured_RR_WorldApriltag.as_euler('xyz')
+print(measured_rv)
+
+measured_t_WorldApriltag = measured_X_WorldApriltag[:3,3]
+print(measured_t_WorldApriltag)
+
+pose_err = 3.619507164899972e-08 # Hard coded for now
+
+# https://stackoverflow.com/a/42723571
+#v, _ = Rodrigues(X_WorldCube.dot(measured_X_WorldCube.T))
+#
+"""m = X_WorldApriltag @ measured_X_WorldApriltag.T
+print(m)
+transformation_error_from_identity = np.linalg.norm(m)
+print('Transformation error from identity:', transformation_error_from_identity)
+
+dm = X_WorldApriltag - measured_X_WorldApriltag
+print(dm)
+
+m = X_WorldApriltag.T @ measured_X_WorldApriltag
+print(m)
+transformation_error_from_identity = np.linalg.norm(m)
+print('Transformation error from identity:', transformation_error_from_identity)"""
+
+r = np.linalg.norm(R_WorldApriltag @ measured_R_WorldApriltag.T)
+print(r)
+
+t = np.linalg.norm(t_WorldApriltag - measured_t_WorldApriltag)
+print(t)
+
+print(rv - measured_rv)
+rv_norm = np.linalg.norm(rv - measured_rv, 1) / 3
+print(rv_norm)
