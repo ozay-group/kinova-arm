d5050935a179677fae252a2c4db2c34411a38c03
kwesiRutledge <krutledg@umich.edu>
2022-07-30 21:15:16 +0000

    Changed data input/output format.

--- drake/vision_calibration_apriltag/test/pose_verification.py
+++ drake/vision_calibration_apriltag/test/pose_verification.py
@@ -21,7 +21,7 @@ a = 0.0762
 ## If the opposite face of the Apriltag matches the center of the gripper and
 ## it is kept parallel to the interface.
 X_EndeffectorApriltag = np.identity(4)
-X_EndeffectorApriltag[2,3] = z + a
+X_EndeffectorApriltag[2,3] = 0#z + a
 
 ## Find the pose of the Apriltag in the world frame.
 X_WorldApriltag = X_WorldEndeffector @ X_EndeffectorApriltag
@@ -34,13 +34,15 @@ r_WorldApriltag = R.from_matrix(R_WorldApriltag).as_euler('xyz') # numpy array (
 ## Load the measured pose of the Intel Realsense camera.
 X_WorldRealsense = np.load('/root/kinova-arm/drake/vision_calibration_apriltag/X_WorldRealsense.npy')
 
-## Hard code the rotation matrix and translation vector from "realsense_result.txt"
-R_RealsenseApriltag = np.array([[ 0.99559036,  0.08261408,  0.04443809],
- [-0.07829508,  0.99272777, -0.09144102],
- [-0.05166924,  0.08755852,  0.99481847]])
-t_RealsenseApriltag = np.array([[0.07921371],
- [-0.12779749],
- [ 0.37730513]])
+## Hard code the rotation matrix and translation vector from realsense_result
+R_RealsenseApriltag = np.load("pose_R_realsense.npy")
+t_RealsenseApriltag = np.load("pose_t_realsense.npy")
+"""R_RealsenseApriltag = np.array([[ 0.59212563,  0.01627912, -0.80568122],
+ [-0.28292787,  0.94034869, -0.18893427],
+ [ 0.7545456,   0.3398225,   0.56141038]])
+t_RealsenseApriltag = np.array([[-0.12280271],
+ [ 0.02189387],
+ [ 0.5466977 ]])"""
 X_RealsenseApriltag = np.concatenate((R_RealsenseApriltag, t_RealsenseApriltag), axis=1)
 X_RealsenseApriltag = np.concatenate((X_RealsenseApriltag, np.array([[0, 0, 0, 1]])), axis=0)
 
