1bccc177d00a74b460e6c6a2547fb61450bafed9
daixingze <xngzdai@umich.edu>
2022-08-09 04:12:55 +0000

    Enabled to view 2D boundaries (no rotation).

--- drake/bounce_pympc/default_params.py
+++ drake/bounce_pympc/default_params.py
@@ -23,7 +23,7 @@ xn_min = np.zeros(10)                                                           
 
 # terminal set
 # X_N = Polyhedron.from_bounds(*[np.zeros(10)]*2)
-X_N = Polyhedron.from_bounds(x_min, x_max)
+X_N = Polyhedron.from_bounds(xn_min, xn_max)
 
 # input bounds
 u_max = np.array([
@@ -49,3 +49,53 @@ R = np.diag([
 P = np.zeros((10, 10))
 
 coeff_rest = 1
+
+if __name__ == "__main__":
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    plt.xlabel("x (m)")
+    plt.ylabel("z (m)")
+
+    # Get the current reference
+    ax = plt.gca()
+
+    # extract the positional bounds
+    p_index = [0, 1, 3, 4]
+    x_max_p = np.array([x_max[i] for i in p_index])
+    x_min_p = np.array([x_min[i] for i in p_index])
+    xn_max_p = np.array([xn_max[i] for i in p_index])
+    xn_min_p = np.array([xn_min[i] for i in p_index])
+
+    # plot safety region
+    safety_region = np.abs(x_max_p - x_min_p)
+    safety_region_b = matplotlib.patches.Rectangle((x_min_p[0], x_min_p[1]), safety_region[0], safety_region[1], 
+        color="magenta", alpha=1, fill=False, ls=':', lw=2.0, label="ball safety region")
+    ax.add_patch(safety_region_b)
+    safety_region_f = matplotlib.patches.Rectangle((x_min_p[2], x_min_p[3]), safety_region[2], safety_region[3], 
+        color="green", alpha=0.5, fill=False, lw=2.0, label="floor safety region")
+    ax.add_patch(safety_region_f)
+
+    # plot target region
+    target_region = np.abs(xn_max_p - xn_min_p)
+    target_region_b = matplotlib.patches.Rectangle((xn_min_p[0], xn_min_p[1]), target_region[0], target_region[1], 
+        color="blue", alpha=1, fill=False, ls=':', lw=2.0, label="ball target region")
+    ax.add_patch(target_region_b)
+    target_region_f = matplotlib.patches.Rectangle((xn_min_p[2], xn_min_p[3]), target_region[2], target_region[3], 
+        color="red", alpha=0.5, fill=False, lw=2.0, label="floor target region")
+    ax.add_patch(target_region_f)
+
+    # draw the axis
+    p_mat = np.abs(np.hstack((x_max_p, xn_max_p, x_min_p, xn_min_p)))
+    lim = np.max(p_mat) * 1.1
+    ax.set_xlim([-lim, lim])
+    ax.set_ylim([-lim, lim])
+    plt.legend()
+
+    # show/save the figure
+    if matplotlib.get_backend() == "agg":
+        plt.savefig("safety_regions.png")
+    else:
+        plt.show()
