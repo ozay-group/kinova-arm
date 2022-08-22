42710b0b328874b9d7bb12eb7b2e03a08964e794
kwesiRutledge <krutledg@umich.edu>
2022-08-22 20:04:56 +0000

    Xingze: Realized the Camera LeafSystem on hardware

--- drake/bounce_pympc/test/camera_test.py
+++ drake/bounce_pympc/test/camera_test.py
@@ -26,16 +26,25 @@ class Camera(LeafSystem):
 
         self.ball_radius = 0.02 # in meters
 
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+        ## --- Start streaming --- ##
+        self.profile = self.pipeline.start(self.config)
+
         # Get intrinsics of the depth stream
         depth_profile = self.profile.get_stream(rs.stream.depth)
-        intr = depth_profile.as_video_stream_profile().get_intrinsics()
-        self.intr_m = np.array([[intr.fx, 0, intr.ppx], [0, intr.fy, intr.ppy], [0, 0, 1]])
+        self.intr = depth_profile.as_video_stream_profile().get_intrinsics()
+        self.intr_m = np.array([[self.intr.fx, 0, self.intr.ppx], [0, self.intr.fy, self.intr.ppy], [0, 0, 1]])
         self.extr_m = np.load('X_WorldRealsense.npy')
 
         # Getting the depth sensor's depth scale (see rs-align example for explanation)
         depth_sensor = self.profile.get_device().first_depth_sensor()
         self.depth_scale = depth_sensor.get_depth_scale()
-        # print("Depth Scale is: " , depth_scale)
+        print("Depth Scale is: " , self.depth_scale)
 
         # We will be removing the background of objects more than
         #  clipping_distance_in_meters meters away
@@ -56,32 +65,14 @@ class Camera(LeafSystem):
         # Update ball position with discrete period according to function DoUpdate
         self.DeclarePeriodicDiscreteUpdateEvent(self.period, 0, self.DoUpdate)
         self.state_output_port = self.DeclareStateOutputPort("ball_state", self.state_index)
-    
-    @staticmethod
-    def __enter__(self):
-        # Setup camera connection
-        self.pipeline = rs.pipeline()
-        self.config = rs.config()
-
-        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-        ## --- Start streaming --- ##
-        self.profile = self.pipeline.start(self.config)
-
-        return self
-    
-    @staticmethod
-    def __exit__(self, exc_type, exc_value, traceback):
-        if traceback is not None:
-            print(exc_type, exc_value, traceback)
-        self.pipeline.stop()
-        return self
 
     def _find_location(self, f_m, intrinsic_m, extrinsic_m, depth_scale):
-            frame_coordinate = np.array([f_m[0], f_m[1], f_m[2]*depth_scale + self.ball_radius, 1])
-            X_CameraBall = np.eye(4)
-            X_CameraBall[0:3, 0:3] = intrinsic_m
-            X_B = extrinsic_m @ X_CameraBall @ frame_coordinate
+            frame_coordinate = np.array([f_m[0], f_m[1], f_m[2]*depth_scale + self.ball_radius])
+            X_CameraBall = frame_coordinate @ intrinsic_m
+            X_CameraBall = np.append(X_CameraBall,[1.0])
+            # X_CameraBall = np.eye(4)
+            # X_CameraBall[0:3, 0:3] = intrinsic_m
+            X_B = extrinsic_m @ X_CameraBall
             return X_B
 
     def DoUpdate(self, context, xb):
@@ -92,6 +83,7 @@ class Camera(LeafSystem):
 
         # Wait for a coherent pair of frames: depth and color
         frames = self.pipeline.wait_for_frames()
+        print("wait for frames()")
 
         # Align the depth frame to color frame
         aligned_frames = self.align.process(frames)
@@ -128,20 +120,24 @@ class Camera(LeafSystem):
         rows = filtered_blurred_gray_image.shape[0]
         circles = cv2.HoughCircles(filtered_blurred_gray_image, cv2.HOUGH_GRADIENT, 0.5, rows/8, param1=120, param2=15, minRadius=0, maxRadius=-1)
 
+        world_corrdinate = np.array([0, 0, 0])
         if circles is not None:
             circles= np.uint16(np.around(circles))
             major_circle = circles[0][0]
-            center = (major_circle[0],major_circle[1])
-            try:
-                point_px_new = np.array([center[0], center[1], depth_image[center[0],center[1]]]) # (x, y, distance by (x, y)) measured in pixels
-                if point_px_new[2] != 0:
-                    point_px = point_px_new
-                    # cv2.circle(filtered_rgb_image, center, 1, (0,255,255),3)
-                    # print(point_px)
-                    world_corrdinate = self._find_location(point_px, self.intr_m, self.extr_m, self.depth_scale)
-                # print(world_corrdinate)
-            except IndexError:
-                world_corrdinate = None
+            center = [major_circle[0],major_circle[1]]
+            X_CameraBall = rs.rs2_deproject_pixel_to_point(self.intr, center, self.depth_scale)
+            world_corrdinate = self.extr_m @ np.append(X_CameraBall, [1])
+            print(world_corrdinate)
+            # try:
+            #     point_px_new = np.array([center[0], center[1], depth_image[center[0],center[1]]]) # (x, y, distance by (x, y)) measured in pixels
+            #     if point_px_new[2] != 0:
+            #         point_px = point_px_new
+            #         # cv2.circle(filtered_rgb_image, center, 1, (0,255,255),3)
+            #         # print(point_px)
+            #         world_corrdinate = self._find_location(point_px, self.intr_m, self.extr_m, self.depth_scale)
+            #     # print(world_corrdinate)
+            # except IndexError:
+            #     world_corrdinate = None
 
         # Four cases:
         # 1. The ball's position has not been initialized   AND     the ball is not found   --> raise an error
@@ -178,45 +174,42 @@ class Camera(LeafSystem):
 
 
 def demo():
-    # Whether or not to plot the safety/target regions in the meshcat visualizer
-    plot_regions = True
+    # with Camera().__enter__() as Camera:
+        # Whether or not to plot the safety/target regions in the meshcat visualizer
+        plot_regions = True
 
-    # Create a diagram
-    builder = DiagramBuilder()
-    scene_graph = builder.AddSystem(SceneGraph())
+        # Create a diagram
+        builder = DiagramBuilder()
+        scene_graph = builder.AddSystem(SceneGraph())
 
-    # Setup visualization
-    # meshcat = StartMeshcat()
-    # MeshcatVisualizer(meshcat).AddToBuilder(builder, scene_graph, meshcat)
+        # Setup visualization
+        # meshcat = StartMeshcat()
+        # MeshcatVisualizer(meshcat).AddToBuilder(builder, scene_graph, meshcat)
 
-    
-    # Add modules to the diagram
-    cam = Camera(params).AddToBuilder(builder, scene_graph)
-    sink = Sink().AddToBuilder(builder, scene_graph)
+        
+        # Add modules to the diagram
+        cam = Camera(params).AddToBuilder(builder, scene_graph)
+        sink = Sink().AddToBuilder(builder, scene_graph)
 
-    builder.Connect(cam.state_output_port, sink.input_port)
-    
-    # Build the diagram
-    diagram = builder.Build()
+        builder.Connect(cam.state_output_port, sink.input_port)
+        
+        # Build the diagram
+        diagram = builder.Build()
 
-    # Set up a simulator to run this diagram
-    simulator = Simulator(diagram)
-    context = simulator.get_mutable_context()
-    context.SetDiscreteState(params.xb0) # initial context for the ball system.
-    context.SetContinuousState([]) # initial context for the solver (paddle acceleration)
+        # Set up a simulator to run this diagram
+        simulator = Simulator(diagram)
+        context = simulator.get_mutable_context()
+        context.SetDiscreteState(params.xb0) # initial context for the ball system.
+        context.SetContinuousState([]) # initial context for the solver (paddle acceleration)
 
-    # Set the initial conditions
-    # context.SetDiscreteState(params.xb0)
-    # if plot_regions: context.SetDiscreteState(region.def_state_v) # initial context for the region plotting system if Region() is constructed.
-    
-    # Try to run simulation 4 times slower than real time
-    simulator.set_target_realtime_rate(0.25)
-    try:
-        simulator_status = simulator.Initialize()
-        simulator.AdvanceTo(float(100))
-    except RuntimeError:
-        print(simulator_status.message())
-        return
+        # Set the initial conditions
+        # context.SetDiscreteState(params.xb0)
+        # if plot_regions: context.SetDiscreteState(region.def_state_v) # initial context for the region plotting system if Region() is constructed.
+        
+        # Try to run simulation 4 times slower than real time
+        simulator.set_target_realtime_rate(0.25)
+        simulator.set_publish_every_time_step(True)
+        simulator.AdvanceTo(0.5)
     
 
 if __name__ == "__main__":
