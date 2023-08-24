7bff6f3dbfbe9cd59c56c04bad1720f6d81a1d09
Kwesi Rutledge <kwesir@mit.edu>
2023-08-24 17:25:10 -0400

    Deleted Some Older Dockerfiles that I don't understand anymore

--- shell-scripts/create-drake-v5-docker-image.bat
+++
@@ -1,9 +0,0 @@
-:: create-drake-v4-docker-image.bat
-:: Description: create the docker image for the project
-::   
-
-:: Pull the latest public Docker image for Drake
-docker pull robotlocomotion/drake:latest
-
-:: Docker build the image
-docker build -t drake-image-v5 -f docker/drake_v5/Dockerfile .
